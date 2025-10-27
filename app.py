48a9bc1418ab46bdb625367cce1baabc136767a3
AP <adonis.paz@grupobusiness.it>
2025-10-27 18:06:23 -0500

    paso

--- app.py
+++ app.py
@@ -13,21 +13,21 @@ def call_app_b():
     except requests.exceptions.ConnectionError:
         return 'Error: no se pudo conectar con app B', 500
 @app.route('/health', methods=['GET'])
-def call_app_b():
+def call_health():
     try:
         # Llamamos a la app B, que está en localhost:3001
         return 'OK', response.status_code
     except requests.exceptions.ConnectionError:
         return 'Error: no se pudo conectar con app B', 500
 @app.route('/startup', methods=['GET'])
-def call_app_b():
+def call_startup():
     try:
         # Llamamos a la app B, que está en localhost:3001
         return 'OK', response.status_code
     except requests.exceptions.ConnectionError:
         return 'Error: no se pudo conectar con app B', 500
 @app.route('/readiness', methods=['GET'])
-def call_app_b():
+def call_readiness():
     try:
         # Llamamos a la app B, que está en localhost:3001
         return 'OK', response.status_code
